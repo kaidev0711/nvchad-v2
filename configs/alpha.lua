0eb69118488c58627096c528afa35841bd88d208
kaidev0711 <kaiz.developers@gmail.com>
2023-04-23 18:28:45 +0700

    projects

--- configs/alpha.lua
+++ configs/alpha.lua
@@ -27,7 +27,7 @@ dashboard.section.header.val = board
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+  dashboard.button("p", "  Find project", ":lua require'telescope'.extensions.projects.projects{}<CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
   dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
